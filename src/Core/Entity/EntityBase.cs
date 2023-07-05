65aa439d3d5358b7ef2e6508dab5de7da2e6b2eb
Victor <vicosoft4real@gmail.com>
2023-07-05 04:24:02 +0100

    Remove docker compose

--- src/Core/Entity/EntityBase.cs
+++ src/Core/Entity/EntityBase.cs
@@ -1,5 +1,9 @@
 namespace Core.Entity;
 
+/// <summary>
+/// Base class for all entities
+/// </summary>
+/// <typeparam name="TId"></typeparam>
 public abstract class EntityBase<TId>
 {
     protected EntityBase(TId id)
